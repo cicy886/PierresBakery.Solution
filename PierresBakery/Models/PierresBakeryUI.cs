b552abfd93c43bb659e50639404463ebd8f39fa1
Sisi Vieira <cicy886@gmail.com>
2021-09-11 13:58:10 -0700

    BreadPriceCheck objects of the BreadPriceCheck type created successfully

--- PierresBakery/Models/PierresBakeryUI.cs
+++ PierresBakery/Models/PierresBakeryUI.cs
@@ -14,6 +14,7 @@ namespace PierresBakery.Models
       Console.WriteLine("Bread: Buy 2, get 1 free (every 3rd loaf of bread is free.). A single loaf costs $5, two loaves costs $10, and three loaves cost $10.");
       Console.WriteLine("Pastry: Buy 1 for $2 or 3 for $5. Four pastries costs $7, five pastries costs $9, and six pastries costs $10.");
       Console.WriteLine("Please press 1 to choose Bread, press 2 to choose pastry:");
+      string chooseBreadOrPastry = Console.ReadLine();
     }
   }
 }
