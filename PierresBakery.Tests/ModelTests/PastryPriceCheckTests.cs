dbebc76d23533130d2835bb3518864fd184cef3f
Sisi Vieira <cicy886@gmail.com>
2021-09-11 14:00:44 -0700

    PastryPriceCheck objects of the PastryPriceCheck type created successfully

---
+++ PierresBakery.Tests/ModelTests/PastryPriceCheckTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PastryPriceCheck.Models;
+
+namespace PastryPriceCheck.Tests
+{
+  [TestClass]
+  public class PastryTests
+  {
+    [TestMethod]
+    public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
+    {
+      Pastry newPastry = new Pastry();
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
+  }
+}
---
+++ PierresBakery/Models/PastryPriceCheck.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace PastryPriceCheck.Models
+{
+  public class Pastry
+  {
+
+  }
+}
