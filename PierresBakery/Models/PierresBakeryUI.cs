4202ed8ab66904215982f1e786e49ede4e12391d
Sisi Vieira <cicy886@gmail.com>
2021-09-11 13:31:55 -0700

    add ui in .cs file  and add outputtype in .csporj file

--- PierresBakery/Models/PierresBakeryUI.cs
+++ PierresBakery/Models/PierresBakeryUI.cs
@@ -1,9 +1,19 @@
+using System;
+
 namespace PierresBakery.Models
 {
   public class PierresBakeryUI
   {
-
-    // properties, methods, etc. will go here.
-
+    public static void Main ()
+    {
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("The products available in Pierre's Bakery are:");
+      Console.WriteLine ("1. Bread.");
+      Console.WriteLine("2. Pastry.");
+      Console.WriteLine("We are having a promotion now! Here are the prices:");
+      Console.WriteLine("Bread: Buy 2, get 1 free (every 3rd loaf of bread is free.). A single loaf costs $5, two loaves costs $10, and three loaves cost $10.");
+      Console.WriteLine("Pastry: Buy 1 for $2 or 3 for $5. Four pastries costs $7, five pastries costs $9, and six pastries costs $10.");
+      Console.WriteLine("Please press 1 to choose Bread, press 2 to choose pastry:");
+    }
   }
 }
