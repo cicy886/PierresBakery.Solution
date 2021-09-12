b17bd08d2906537b9fae9f41819bb6e1d4fee1c5
Sisi Vieira <cicy886@gmail.com>
2021-09-11 18:09:41 -0700

    test price for 1 bread. test passed.

--- PierresBakery.Tests/ModelTests/BreadPriceCheckTests.cs
+++ PierresBakery.Tests/ModelTests/BreadPriceCheckTests.cs
@@ -24,6 +24,83 @@ namespace BreadPriceCheck.Tests
       //Assert
       Assert.AreEqual(1, newBread.BreadAmount);
     }
-  }
+    [TestMethod]
+      public void SetPriceForBreadOne_SetPrice_Int()
+      {
+        //Arrange
+        int breadAmount = 1;
 
+        //Act
+        Bread newBread = new Bread(breadAmount);
+        newBread.SetTotalPrice();
+
+        //Assert
+        Assert.AreEqual(5, newBread.SetTotalPrice());
+      }
+      //     [TestMethod]
+      // public void SetPriceForBreadTwo_SetPrice_Int()
+      // {
+      //   //Arrange
+      //   int breadAmount = 2;
+
+      //   //Act
+      //   Bread newBread = new Bread(breadAmount);
+      //   newBread.SetTotalPrice();
+
+      //   //Assert
+      //   Assert.AreEqual(10, newBread.SetTotalPrice());
+      // }
+      //     [TestMethod]
+      // public void SetPriceForBreadThree_SetPrice_Int()
+      // {
+      //   //Arrange
+      //   int breadAmount = 3;
+
+      //   //Act
+      //   Bread newBread = new Bread(breadAmount);
+      //   newBread.SetTotalPrice();
+
+      //   //Assert
+      //   Assert.AreEqual(10, newBread.SetTotalPrice());
+      // }
+      //     [TestMethod]
+      // public void SetPriceForBreadFour_SetPrice_Int()
+      // {
+      //   //Arrange
+      //   int breadAmount = 4;
+
+      //   //Act
+      //   Bread newBread = new Bread(breadAmount);
+      //   newBread.SetTotalPrice();
+
+      //   //Assert
+      //   Assert.AreEqual(15, newBread.SetTotalPrice());
+      // }
+      //     [TestMethod]
+      // public void SetPriceForBreadFive_SetPrice_Int()
+      // {
+      //   //Arrange
+      //   int breadAmount = 5;
+
+      //   //Act
+      //   Bread newBread = new Bread(breadAmount);
+      //   newBread.SetTotalPrice();
+
+      //   //Assert
+      //   Assert.AreEqual(20, newBread.SetTotalPrice());
+      // }
+      //     [TestMethod]
+      // public void SetPriceForBreadSix_SetPrice_Int()
+      // {
+      //   //Arrange
+      //   int breadAmount = 6;
+
+      //   //Act
+      //   Bread newBread = new Bread(breadAmount);
+      //   newBread.SetTotalPrice();
+
+      //   //Assert
+      //   Assert.AreEqual(20, newBread.SetTotalPrice());
+      // }
+  }
 }
