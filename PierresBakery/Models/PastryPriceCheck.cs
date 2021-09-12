f388dc4842cefc33bb1868ba7cb2b172f6d86386
Sisi Vieira <cicy886@gmail.com>
2021-09-11 18:37:18 -0700

    add logic to check pastry amount

--- PierresBakery/Models/PastryPriceCheck.cs
+++ PierresBakery/Models/PastryPriceCheck.cs
@@ -4,6 +4,28 @@ namespace PastryPriceCheck.Models
 {
   public class Pastry
   {
+    public int PastryAmount {get; set;}
+    public int Price {get; set;}
 
+    public Pastry(int pastryAmount, int price=0)
+    {
+      PastryAmount = pastryAmount;
+      Price = price;
+    }
+
+    // public int SetTotalPrice()
+    // {
+    //   //Pastry: Buy 1 for $2 or 3 for $5. Four pastries costs $7, five pastries costs $9, and six pastries costs $10.
+    //   if (PastryAmount % 3 == 0) 
+    //   {
+    //     Price = PastryAmount/3*5;
+    //   } 
+    //   else if (PastryAmount %3 != 0)
+    //   {
+    //     Price = (PastryAmount/3*5) + (PastryAmount % 3 * 2);
+    //   } 
+
+    //   return Price;
+    // }
   }
 }
