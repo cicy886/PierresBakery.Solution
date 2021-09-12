58b9c2cfedfa9db94425e8f81ce760057821485f
Sisi Vieira <cicy886@gmail.com>
2021-09-11 18:43:25 -0700

    test get pastry amount. test passed.

--- PierresBakery.Tests/ModelTests/PastryPriceCheckTests.cs
+++ PierresBakery.Tests/ModelTests/PastryPriceCheckTests.cs
@@ -9,8 +9,20 @@ namespace PastryPriceCheck.Tests
     [TestMethod]
     public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
     {
-      Pastry newPastry = new Pastry();
+      Pastry newPastry = new Pastry(1);
       Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
+    [TestMethod]
+    public void SetAmountOfPastryToBuy_SetAmount_Int()
+    {
+      //Arrange
+      int pastryAmount = 1;
+
+      //Act
+      Pastry newPastry = new Pastry(pastryAmount);
+
+      //Assert
+      Assert.AreEqual(1, newPastry.PastryAmount);
     }
   }
 }
